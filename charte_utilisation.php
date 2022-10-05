24a48c3fe133e9f75eb292408751a5e1bffeb00a
Applencourt Samuel <Samuel.Applencourt@uphf.fr>
2022-10-05 10:59:46 +0200

    Ajout charte_utilisation.php

---
+++ charte_utilisation.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang=en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="AideCss.css">
+
+
+</head>
+<body>
