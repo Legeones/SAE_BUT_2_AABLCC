05ca9f835b05230ec056e9629787ba3d9a83f7ae
LaurentCharlier <103184437+LaurentCharlier@users.noreply.github.com>
2022-10-05 10:03:47 +0200

    newVersionPHPInscription/connexion/chgmdp+css

--- inscription.php
+++ inscription.php
@@ -1,29 +1,86 @@
 <?php
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
+ini_set('SMTP','smtp.gmail.com');
+ini_set('smtp_port',587);
 
-function VerifPassword_Equality($password,$password2)
+function VerifEmptyContent($text)
 {
-    if ($password != $password2)      { return 0; }
-    else {return 1;}
+    if ($text == "")
+    { return 0; }
+    else
+    { return 1; }
 }
 
-function VerifPassword($password,$password2)
+function VerifPassword_Equality($pw1,$pw2)
 {
-    try
-    {
-        VerifPassword_Equality($password,$password2);
-    }
-    catch (Exception $e) { echo 'Exception recue : ',  $e->getMessage(), "\n"; }
+    if ( ($pw1 != $pw2) != 0 )
+    { return 0; }
+    else
+    { return 1; }
 }
 
-$res=VerifPassword_Equality($_POST["Password_A"], $_POST["Password_B"]);
+function VerifPassword_Lenght($pw1)
+{
+    if ( strlen($pw1) < 8 )
+    { return 0; }
+    else
+    { return 1; }
+}
 
-if($res==1) {
-    $db_username = 'db_username';
-    $db_password = 'db_pass';
-    $db_name = 'db_name';
-    $db_host = 'db_host';
+function VerifPassword_Uppercase($pw1)
+{
+    $pw1seg = str_split($pw1);
+    $uppercasecheck = false;
+
+    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
+    { if(preg_match('/[A-Z]/', $pw1seg[$i]) == 1)  { $uppercasecheck = true;   /*echo "UpperCase Found <br>";*/ } }
+
+    if ( $uppercasecheck == false )
+    { return 0; }
+    else
+    { return 1; }
+}
+
+function VerifPassword_Lowercase($pw1)
+{
+    $pw1seg = str_split($pw1);
+    $lowercasecheck = false;
+
+    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
+    { if(preg_match('/[a-z]/', $pw1seg[$i]) == 1)  { $lowercasecheck = true;   /*echo "LowerCase Found <br>";*/ } }
+
+    if ( $lowercasecheck == false )
+    { return 0; }
+    else
+    { return 1; }
+}
+
+function VerifPassword_Number($pw1)
+{
+    $pw1seg = str_split($pw1);
+    $numbercheck = false;
+
+    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
+    { if(preg_match('/[0-9]/', $pw1seg[$i]) == 1)  { $numbercheck = true;   /*echo "Number Found <br>";*/ } }
+
+    if ( $numbercheck == false )
+    { return 0; }
+    else
+    { return 1; }
+}
+$resVerifPassword_Uppercase=VerifPassword_Uppercase($_POST["Password_A"]);
+$resVerifPassword_Number=VerifPassword_Number($_POST["Password_A"]);
+$resVerifPassword_Equality=VerifPassword_Equality($_POST["Password_A"], $_POST["Password_B"]);
+$resVerifPassword_Lenght=VerifPassword_Lenght($_POST["Password_A"]);
+$resVerifPassword_Lowercase=VerifPassword_Lowercase($_POST["Password_A"]);
+
+if($resVerifPassword_Lowercase==1 and $resVerifPassword_Equality==1 and $resVerifPassword_Lenght==1 and $resVerifPassword_Uppercase==1 and $resVerifPassword_Number==1) {
+    $db_username = 'iutinfo86';
+    $db_password = 'pmD5t+DV';
+    $db_name = 'iutinfo86';
+    $db_host = 'iutinfo-sgbd.uphf.fr';
 
     $options = [
         'cost' => 12,
@@ -33,10 +90,11 @@ if($res==1) {
 
     try {
         $dbh = new PDO("pgsql:host=$db_host;port=5432;dbname=$db_name;user=$db_username;password=$db_password");
-        $stmt = $dbh->prepare("INSERT INTO utilisateur values (?,?,?)");
+        $stmt = $dbh->prepare("INSERT INTO utilisateur values (?,?,?,?)");
         $stmt->bindParam(1, $_POST["ID"]);
         $stmt->bindParam(2, $mdpHacher);
-        $stmt->bindParam(3, $_POST['Role']);
+        $stmt->bindParam(3, $_POST['email']);
+        $stmt->bindParam(4, $_POST['Role']);
 
         $stmt->execute();
         header('Location: login.php');
@@ -46,6 +104,24 @@ if($res==1) {
     }
 }
 
+if ($resVerifPassword_Equality==0){
+    header('Location: Inscription_formulaire.php?erreur=2');
+}
 
+elseif ($resVerifPassword_Lenght==0){
+    header('Location: Inscription_formulaire.php?erreur=1');
+}
+
+elseif($resVerifPassword_Lowercase==0){
+    header('Location: Inscription_formulaire.php?erreur=3');
+}
+
+elseif($resVerifPassword_Number==0){
+    header('Location: Inscription_formulaire.php?erreur=4');
+}
+
+elseif($resVerifPassword_Uppercase==0) {
+    header('Location: Inscription_formulaire.php?erreur=5');
+}
 
 ?>
