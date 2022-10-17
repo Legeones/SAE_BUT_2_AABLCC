a37baebe9895feb633beb9534f25b223e146eb3c
Asbean <82886602+Asbean@users.noreply.github.com>
2022-10-17 16:08:29 +0200

    Added Database Launcher

--- Verification_KeyCode.php
+++ Verification_KeyCode.php
@@ -1,12 +1,20 @@
 <?php
+
+require('DataBase.php');
+
 function Key_Validation()
 {
     session_start();
     
     if ( $_POST['Key'] == $_SESSION['Code'] )
-    { header('Location: MailCode_Formulaire.php?after=0'); }
+    {
+        header('Location: MailCode_Formulaire.php?after=0');
+        DataBase_Add_User();
+    }
     else
-    { header('Location: MailCode_Formulaire.php?after=1'); }
+    {
+        header('Location: MailCode_Formulaire.php?after=1');
+    }
 }
 
 Key_Validation();
