0fbdf392a34b26f677c0233aff7091db490c5c04
Asbean <82886602+Asbean@users.noreply.github.com>
2022-10-19 14:39:52 +0200

    Update Verification_PasswordChange.php

--- MDP/Verification_PasswordChange.php
+++ MDP/Verification_PasswordChange.php
@@ -2,6 +2,41 @@
 
 require('../BDD/DataBase.php');
 
-DataBase_User_New_Pass_Check($_POST['id'],$_POST['ad']);
+function VerifEmail($email)
+{
+    $email = filter_var($email,FILTER_SANITIZE_EMAIL);
+    
+    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    { return 1; }
+    else
+    { return 0; }
+}
+
+function VerifEmptyContent($text)
+{
+    if ($text == "")
+    { return 0; }
+    else
+    { return 1; }
+}
+
+$resVerifemptymail = VerifEmptyContent($_POST['mail']);
+$resVerifemptyusername = VerifEmptyContent($_POST['username']);
+$resVerifMail = VerifEmail($_POST['mail']);
+
+if ( $resVerifMail == 0 )
+{ header('Location: ../MDP/MDPoublier.php?erreur=1'); }
+
+if ( $resVerifemptymail == 0 )
+{ header('Location: ../MDP/MDPoublier.php?erreur=2'); }
+
+if ( $resVerifemptyusername == 0 )
+{ header('Location: ../MDP/MDPoublier.php?erreur=2'); }
+
+else 
+{
+    DataBase_User_New_Pass_Check($_POST['username'],$_POST['mail']);
+}
+
 
 ?>
