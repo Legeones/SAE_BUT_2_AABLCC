69d1ecdf43761b94b9dd51bf81fcefebffdbe3f8
Applencourt Samuel <Samuel.Applencourt@uphf.fr>
2022-10-19 15:51:03 +0200

    Deconnexion
    Création de la page de déconnexion

---
+++ Connexion/Deconnexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang=en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page de déconnexion</title>
+    <!-- importation des fichiers de style -->
+    <link rel="stylesheet" href="Deconnexion.css">
+
+<form>
+    <h1>Vous avez été déconnecté avec succès </h1>
+
+</form>
+</head>
+<body>
