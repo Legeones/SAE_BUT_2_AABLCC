1a5c1e13d8ea79634265ae519d449f2f54464244
Asbean <82886602+Asbean@users.noreply.github.com>
2022-10-19 15:08:24 +0200

    Update New_mdp.php

--- MDP/New_mdp.php
+++ MDP/New_mdp.php
@@ -2,7 +2,7 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
-require('../BDD/DataBase.php');
+require('../BDD/DataBase_User.php');
 
 function VerifPassword_Equality($pw1,$pw2)
 {
