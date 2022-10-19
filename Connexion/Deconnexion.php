9f57cee6ed104af179da125e4697c8505fa323cc
Applencourt Samuel <Samuel.Applencourt@uphf.fr>
2022-10-19 16:11:32 +0200

    amélioration page Deconnexion

--- Connexion/Deconnexion.php
+++ Connexion/Deconnexion.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="Deconnexion.css">
 
 <form>
-    <h1>Vous avez été déconnecté avec succès </h1>
-
+    <h1>Vous avez été déconnecté avec succès</h1>
+    <header>
+        <img class="logo" src="https://moodle.uphf.fr/pluginfile.php/358899/mod_resource/content/1/logoIFSI.png">
+    </header>
 </form>
 </head>
 <body>
