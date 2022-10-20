9efb8ae2c7be6566565c5b99b17118ded28b0f48
Applencourt Samuel <Samuel.Applencourt@uphf.fr>
2022-10-20 11:45:42 +0200

    création page d'accueil

---
+++ Connexion/Page_Accueil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang=en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page de déconnexion</title>
+    <!-- importation des fichiers de style -->
+    <link rel="stylesheet" href="Page_Accueil.css">
+
+    <form>
+        <h1>Bienvenue à l'IFSI de Maubeuge</h1>
+        <header>
+            <img class="logo" src="https://moodle.uphf.fr/pluginfile.php/358899/mod_resource/content/1/logoIFSI.png">
+        </header>
+
+        <div class="piedDePage">
+            <div class="Validation" align="center" >
+                <input type="submit" value="Suivant">
+            </div>
+        </div>
+        <div class="piedDePage2">
+            <div class="droite">
+                <input type="button" onclick="window.history.back();return false;" value="Retour"/>
+            </div>
+        </div>
+
+
+    </form>
+</head>
+<body>
