0f5eb0eb8f9c80576e83398ed4bea9aaf9fe8648
Asbean <82886602+Asbean@users.noreply.github.com>
2022-10-21 08:47:14 +0200

    supression redondance

--- MDP/New_mdp.php
+++ MDP/New_mdp.php
@@ -3,64 +3,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
 require('../BDD/DataBase_User.php');
-
-function VerifPassword_Equality($pw1,$pw2)
-{
-    if ( ($pw1 != $pw2) != 0 )
-    { return 0; }
-    else
-    { return 1; }
-}
-
-function VerifPassword_Lenght($pw1)
-{
-    if ( strlen($pw1) < 8 )
-    { return 0; }
-    else
-    { return 1; }
-}
-
-function VerifPassword_Uppercase($pw1)
-{
-    $pw1seg = str_split($pw1);
-    $uppercasecheck = false;
-    
-    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
-    { if(preg_match('/[A-Z]/', $pw1seg[$i]) == 1)  { $uppercasecheck = true;   /*echo "UpperCase Found <br>";*/ } }
-    
-    if ( $uppercasecheck == false )
-    { return 0; }
-    else
-    { return 1; }
-}
-
-function VerifPassword_Lowercase($pw1)
-{
-    $pw1seg = str_split($pw1);
-    $lowercasecheck = false;
-    
-    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
-    { if(preg_match('/[a-z]/', $pw1seg[$i]) == 1)  { $lowercasecheck = true;   /*echo "LowerCase Found <br>";*/ } }
-    
-    if ( $lowercasecheck == false )
-    { return 0; }
-    else
-    { return 1; }
-}
-
-function VerifPassword_Number($pw1)
-{
-    $pw1seg = str_split($pw1);
-    $numbercheck = false;
-    
-    for( $i = 0 ; $i < strlen($pw1) ; $i++ )
-    { if(preg_match('/[0-9]/', $pw1seg[$i]) == 1)  { $numbercheck = true;   /*echo "Number Found <br>";*/ } }
-    
-    if ( $numbercheck == false )
-    { return 0; }
-    else
-    { return 1; }
-}
+require('../Verif_Test/Verifiant.php');
 
 $resVerifPassword_Equality=VerifPassword_Equality($_POST["MDP"], $_POST["Re_MDP"]);
 $resVerifPassword_Uppercase=VerifPassword_Uppercase($_POST["MDP"]);
