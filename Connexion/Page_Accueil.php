249a9eb484d54c9a4f24bd5ea61474cd22ae9618
Applencourt Samuel <Samuel.Applencourt@uphf.fr>
2022-10-26 08:25:41 +0200

    modification

--- Connexion/Page_Accueil.php
+++ Connexion/Page_Accueil.php
@@ -12,10 +12,10 @@
             <img class="logo" src="https://moodle.uphf.fr/pluginfile.php/358899/mod_resource/content/1/logoIFSI.png">
         </header>
         </div>
-        <div class="gauche">
+        <div class="droite">
             <input type="button" onclick="window.location.href ='Localisation.php';" value="Ou nous trouver ?"/>
         </div>
-        <div class="droite">
+        <div class="gauche">
             <input type="button" onclick="window.location.href ='login.php';" value="Connexion "/>
         </div>
     </form>
