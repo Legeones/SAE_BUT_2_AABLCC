cbee5f71cb4c76e616e0101b310a7b623ed6d3e6
Asbean <82886602+Asbean@users.noreply.github.com>
2022-11-09 09:34:08 +0100

    ajout body id general

--- Connexion/login.php
+++ Connexion/login.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 
 </head>
-<body>
+<body id="general">
 <div id="container">
     <!-- zone de connexion -->
 
