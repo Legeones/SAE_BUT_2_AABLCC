1462ab41e2a1f8fb7b8c42b6d93004f370296d72
Leveque Aurélien <Aurelien.Leveque@uphf.fr>
2022-11-23 08:41:49 +0100

    modifier DPI code plus compact

---
+++ DPIpatient/MDPDPI_PHP.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require "RecupInfoBDD_AjouterDPI.php";
+
+for ($i = 2 ; $i<=31 ; $i++) {
+    if ($i == 17 || $i == 18){
+        $i +=1;
+    }
+    $name = 'val'.$i;
+    $_SESSION[$name] = StockDPI()[$i];
+}
+
+
+
+header('Location: MDFDPI.php');
+?>
