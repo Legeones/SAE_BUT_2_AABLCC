e767034a77ad0289b8f2fccb295b9b6187cde31d
Asbean <82886602+Asbean@users.noreply.github.com>
2022-12-01 09:10:55 +0100

    Update RecupCorbeilleSQL.php

--- DPIpatient/RecupCorbeilleSQL.php
+++ DPIpatient/RecupCorbeilleSQL.php
@@ -12,7 +12,7 @@ $VerifPassword_Lowercase=VerifPassword_Lowercase($_POST["IPP_Recup"]);
 if($VerifEmptyContent==0)
 { header('Location: ../DPIpatient/RecupCorbeille.php?erreur=1'); }
 
-elseif ($VerifPassword_Uppercase==1 or $VerifPassword_Lowercase==1)
+elseif ($VerifPassword_Uppercase==1 || $VerifPassword_Lowercase==1)
 { header('Location: ../DPIpatient/RecupCorbeille.php?erreur=3'); }
 
 
