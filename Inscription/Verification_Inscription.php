cba18a7bd40e72e7c1e7b028a14cc6dfd09adcdb
Asbean <82886602+Asbean@users.noreply.github.com>
2022-12-13 11:39:01 +0100

    Ajout Nom+Prenom

--- Inscription/Verification_Inscription.php
+++ Inscription/Verification_Inscription.php
@@ -6,14 +6,16 @@ ini_set('display_errors', true);
 require('../Verif_Test/Mail.php');
 require('../Verif_Test/Verifiant.php');
 
-$resVerifPassword_Uppercase=VerifPassword_Uppercase($_POST["Password_A"]); // Vérification du Password_A
-$resVerifPassword_Number=VerifPassword_Number($_POST["Password_A"]); // Vérification du Password_B
-$resVerifPassword_Equality=VerifPassword_Equality($_POST["Password_A"], $_POST["Password_B"]); // Vérification si le Password_A = Password_B
-$resVerifPassword_Lenght=VerifPassword_Lenght($_POST["Password_A"]); // Vérification de la longueur du Password_A
-$resVerifPassword_Lowercase=VerifPassword_Lowercase($_POST["Password_A"]); // Vérification Password_A
-$VerifEmptyContent1=VerifEmptyContent($_POST["email"]); // Vérification de l'email
-$VerifEmptyContent2=VerifEmptyContent($_POST["ID"]); // Vérification de l'ID
-$VerifEmail=VerifEmail($_POST["email"]); // Vérification de l'email
+$resVerifPassword_Uppercase=VerifPassword_Uppercase($_POST["Password_A"]); // VÃ©rification du Password_A
+$resVerifPassword_Number=VerifPassword_Number($_POST["Password_A"]); // VÃ©rification du Password_B
+$resVerifPassword_Equality=VerifPassword_Equality($_POST["Password_A"], $_POST["Password_B"]); // VÃ©rification si le Password_A = Password_B
+$resVerifPassword_Lenght=VerifPassword_Lenght($_POST["Password_A"]); // VÃ©rification de la longueur du Password_A
+$resVerifPassword_Lowercase=VerifPassword_Lowercase($_POST["Password_A"]); // VÃ©rification Password_A
+$VerifEmptyContent1=VerifEmptyContent($_POST["email"]); // VÃ©rification de l'email
+$VerifEmptyContent2=VerifEmptyContent($_POST["ID"]); // VÃ©rification de l'ID
+$VerifEmptyContent3=VerifEmptyContent($_POST["nom"]); // VÃ©rification du nom
+$VerifEmptyContent4=VerifEmptyContent($_POST["prenom"]); // VÃ©rification du prenom
+$VerifEmail=VerifEmail($_POST["email"]); // VÃ©rification de l'email
 
 session_start();
 $_SESSION['EMAIL'] = $_POST['email'];
@@ -23,10 +25,10 @@ $_SESSION['PASSWORD'] = $_POST["Password_A"];
 
 // Erreur page inscription
 if ($resVerifPassword_Equality==0){
-    header('Location: ../Inscription/Inscription_formulaire.php?erreur=2'); // erreur le mot de passe et sa confirmation sont différents
+    header('Location: ../Inscription/Inscription_formulaire.php?erreur=2'); // erreur le mot de passe et sa confirmation sont diffÃ©rents
 }
 elseif ($resVerifPassword_Lenght==0){
-    header('Location: ../Inscription/Inscription_formulaire.php?erreur=1'); // erreur le mot de passe à moins de 8 caractères
+    header('Location: ../Inscription/Inscription_formulaire.php?erreur=1'); // erreur le mot de passe Ã  moins de 8 caractÃ¨res
 }
 
 elseif($resVerifPassword_Lowercase==0){
@@ -34,7 +36,7 @@ elseif($resVerifPassword_Lowercase==0){
 }
 
 elseif($resVerifPassword_Number==0){
-    header('Location: ../Inscription/Inscription_formulaire.php?erreur=4'); // Erreur numéro
+    header('Location: ../Inscription/Inscription_formulaire.php?erreur=4'); // Erreur numÃ©ro
 }
 
 elseif($resVerifPassword_Uppercase==0) {
@@ -47,6 +49,14 @@ elseif ($VerifEmptyContent1==0){
 
 elseif ($VerifEmptyContent2==0){
     header('Location: ../Inscription/Inscription_formulaire.php?erreur=6'); // Erreur login invalide
+}
+
+elseif ($VerifEmptyContent3==0){
+    header('Location: ../Inscription/Inscription_formulaire.php?erreur=7'); // Erreur Nom vide
+}
+
+elseif ($VerifEmptyContent4==0){
+    header('Location: ../Inscription/Inscription_formulaire.php?erreur=7'); // Erreur Prenom Vide
 }
 
 elseif ($VerifEmail==0){
