d6a0eb130e1a252e5bd1e5851d1af952b52e0d0d
LaurentCharlier <103184437+LaurentCharlier@users.noreply.github.com>
2022-12-16 16:49:49 +0100

    Add files via upload

---
+++ BDD/DataBase_Scenario.php
@@ -0,0 +1,102 @@
+<?php
+require('../BDD/DataBase_Core.php');
+session_start();
+
+function lstderoulanteScenario(){
+    $DPI2 = DataBase_Creator_Unit();
+    $DPI = $DPI2->prepare("select idScenario,nom from Scenario left join ScenarioCorbeille SC on Scenario.idScenario = SC.idSCorb where createur= ? and idSCorb is null");
+    $DPI->bindParam(1,$_SESSION['username']);
+    $DPI->execute();
+    return $DPI;
+}
+
+function lstderoulanteScenarioCorb(){
+    $DPI2 = DataBase_Creator_Unit();
+    $DPI = $DPI2->prepare("select idScenario,nom from ScenarioCorbeille  where createur= ?");
+    $DPI->bindParam(1,$_SESSION['username']);
+    $DPI->execute();
+    return $DPI;
+}
+
+
+function checkScenario($id){
+    try {
+        $dbh = DataBase_Creator_Unit();
+        $stmt2 = $dbh->prepare("SELECT count(*) FROM Scenario WHERE idScenario=? and createur=?");
+        $stmt2->bindParam(1, $id);
+        $stmt2->bindParam(2,$_SESSION['username']);
+        $stmt2->execute();
+        $res= $stmt2->fetchColumn(0);
+        return $res;
+
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+function recupInfoScenario($id){
+    $DPI2 = DataBase_Creator_Unit();
+    $DPI = $DPI2->prepare("select nom,debut,fin,nbev from Scenario where idScenario=? and createur= ?");
+    $DPI->bindParam(1,$id);
+    $DPI->bindParam(2,$_SESSION['username']);
+    $DPI->execute();
+    $liste = array();
+    foreach ($DPI as $value){
+        $liste+=$value;
+    }
+    return $liste;
+}
+
+function recupEvenScenario($id)
+{
+    try {
+        $DPI2 = DataBase_Creator_Unit();
+        $DPI = $DPI2->prepare("select idEvenement from ScenarioEvenement where idScenario=? ");
+        $DPI->bindParam(1, $id);
+        $DPI->execute();
+        $result = $DPI->fetchAll(PDO::FETCH_COLUMN, 0);
+        return $result;
+        //echo "<img class='logo' src=$res>";
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+
+    }
+}
+
+function lstderoulanteEtu($idS)
+{
+    $etu = 'etudiant';
+    try {
+        $DPI2 = DataBase_Creator_Unit();
+        $DPI = $DPI2->prepare("select distinct login,nom,prenom from utilisateur left join ScenarioEtudiant SE on Utilisateur.login = SE.idU
+where roles=? and (idS is null or idS!=?);");
+        $DPI->bindParam(1, $etu);
+        $DPI->bindParam(2,$idS);
+        $DPI->execute();
+        $result = $DPI->fetchAll();
+        return $result;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+
+    }
+}
+
+function insertEvenSceEtu($idS,$idEv,$idEtu,$date){
+    try {
+        $dbh = DataBase_Creator_Unit();
+        $stmt2 = $dbh->prepare("INSERT INTO ScenarioEtudiant values (?,?,?,?)");
+        $stmt2->bindParam(1, $idS);
+        $stmt2->bindParam(2, $idEtu);
+        $stmt2->bindParam(3, $idEv);
+        $stmt2->bindParam(4, $date);
+        $stmt2->execute();
+
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+}
