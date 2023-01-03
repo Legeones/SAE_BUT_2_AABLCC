060ba63311924ffd48b694d32bf27f2c78477140
LaurentCharlier <103184437+LaurentCharlier@users.noreply.github.com>
2023-01-03 11:41:29 +0100

    Add files via upload

---
+++ Scenario/principaleEve.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+    <title>Principal scenario</title>
+    <meta charset="utf-8">
+    <!-- importation des fichiers de style -->
+    <link rel="stylesheet" href="../Verif_Test/CSS_DPI.css" media="screen" type="text/css" />
+</head>
+<body>
+<header>
+    <img class="logo" src="../Images/logoIFSI.png">
+</header>
+<div class="global">
+    <div class="gauche">
+        <div class="profile" id="space-invader">
+            <img width="100%" height="100%" src="https://static.vecteezy.com/ti/vecteur-libre/p3/2318271-icone-de-profil-utilisateur-gratuit-vectoriel.jpg">
+        </div>
+        <div class="btn-group">
+            <button onclick="location.href='../DPIpatient/DPI.php'">PATIENTS</button>
+            <button>SCENARIOS</button>
+            <button>JSAISPAS</button>
+        </div>
+    </div>
+    <div class="droite">
+
+        <?php
+        session_start();
+        if ($_SESSION["Role"] == "admin" or $_SESSION["Role"] == "prof") {echo "<button onclick=location.href='choixScenario.php'>lancer scenario</button>";}
+        ?>
+</body>
+</html>
