8d801cdb60fae9c8d8dee532625f6bd5a0939475
AnselotSteven <Steven.Anselot@uphf.fr>
2023-01-10 10:56:37 +0100

    modif pages et fonction ajout scenario

--- Partie_Scenario/Code.php
+++ Partie_Scenario/Code.php
@@ -1,60 +1,181 @@
+
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     if(!empty($_POST['selectDPI'])){
         $_SESSION['val'] = $_POST['valueipp'];
         if (empty($_SESSION['val'])) {
-            echo "il n'y a pas de données";
+            echo "<p style='color: red'>il n'y a pas de données </p>";
         }
     }
 
-    function takeDPI($dbh)
-    {
-        $dpi = $dbh->prepare("select *
-                            from patient where IPP=?");
-        $id = $_SESSION['val'];
-        $dpi->bindparam(1,$id);
-        $dpi->execute();
-        $donnees = array();
-        foreach($dpi as $row){
-            $donnees += $row;
-        }
-        return($donnees);
+    if(!empty($_POST['type_categorie'])){
+        $_SESSION['type_categorie'] = $_POST['type_categorie'];
+    }else{
+        echo "<p style='color: red'>le type de catégorie est vide</p>";
     }
 
-    function takeColumnPatient($dbh): array
-    {
-        $info = $dbh->prepare("select  column_name
-                        from  information_schema.columns where table_name= ? order by ordinal_position");
-        $val = 'patient';
-        $info->bindparam(1,$val);
-        $info->execute();
-        $donnees = [];
-        foreach ($info as $row){
-            $donnees[] = $row[0];
-        }
-        return $donnees;
+    if(!empty($_POST['nbevent'])){
+        $_SESSION['nbevent'] = $_POST['nbevent'];
+    }else{
+        echo "<p style='color: red'> les données des évènements ne sont pas remplies</p>";
     }
 
-    function modif_dpi($dbh): string
-    {
-        $donnees = takeDPI($dbh);
-        $requete = $dbh->prepare("update patient
-                        set ipp = ?, iep= ?, nom= ?, prenom= ?, ddn= ?, taille_cm= ?, poids_kg= ?, adresse= ?, cp= ?, ville= ?, telpersonnel= ?,
-                        telprofessionnel= ?, allergies= ?, antecedents= ?, obstericaux= ?, domedicaux= ?, dochirurgicaux= ?, idpcon= ?, idptel= ?,
-                        mesuredeprotection= ?, asistantsocial= ?, mdv= ?, synentre= ?, traidomi= ?, dophypsy= ?, mobilite= ?, alimentation= ?,
-                        hygiene= ?, toilette= ?, habit= ?, continence = ?
-                            where ipp = ?");
+    if(!empty($_POST['debut'])){
+        $_SESSION['debut'] = $_POST['debut'];
+    }else{
+        echo "<p style='color: red'>le debut d'évènement est vide </p>";
+    }
 
-        for($i=1; $i<= sizeof($donnees); $i++){
-            $requete->bindparams($i, $_SESSION[$i]);
+    if(!empty($_POST['fin'])){
+        $_SESSION['fin'] = $_POST['fin'];
+    }else{
+        echo "<p style='color: red'>la fin d'évènement est vide </p>";
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//Partie evenement
+
+//fonction qui permet d'avoir la liste des id des évènements
+//                    function id_existance_aleatoire($dbh){
+//                        $req = $dbh->prepare("select idevenement
+//                        from evenement ");
+//                        $req->execute();
+//                        $donnees = array();
+//                        foreach ($req as $r){
+//                            $donnees[] = $r;
+//
+//                        }
+//                        return $donnees;
+//                    }
+
+
+function id_existance_categorie($dbh): array  //fonction qui permet de récupérer les id des évènements par catégorie
+{
+    $req = $dbh->prepare("select idevenement
+                        from evenement where categorie = ? ");
+    $req->bindparam(1, $_POST['type_categorie']);
+    $req->execute();
+    $donnees = array();
+    foreach ($req as $r){
+        $donnees[] = $r;
+
+    }
+    return $donnees;
+}
+
+
+
+function recup_event_id($dbh): array //fonction qui récupère les id min et max des évènements
+{
+    $req = $dbh->prepare("select min(idevenement), max(idevenement) from evenement");
+    $req->execute();
+    $infos = array();
+    foreach ($req as $row){
+        $infos[] = $row;
+    }
+    return $infos;
+}
+
+
+/**
+ * @throws Exception
+ */
+function recup_event($dbh){ //fonction qui permet de récupérer une liste d'évènements en fonction de leur id et catégorie
+    $i = $_SESSION['nbevent'];
+    $k= 0;
+    $donnees = recup_event_id($dbh);
+    $les_ids= id_existance_categorie($dbh);
+    $events = array();
+    if(!empty($i)){
+        while ($k != $i){
+            $r = random_int($donnees[0][0], $donnees[0][1]);
+            if (in_array($r, $les_ids[$k])){
+                $events[] = $r;
+                $k += 1;
+            }
         }
-        $requete->execute();
-        return "fait";
+    }
+    return $events;
+}
 
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//Partie scénario
+
+
+function search_idscenario($db) //fonction qui permet de trouver l'id max des scénarios
+{
+    $src = $db->prepare("select max(idscenario)
+                        from scenario");
+    $src->execute();
+    $donnees = array();
+    foreach ($src as $row){
+        $donnees += $row;
+    }
+    return $donnees[0] + 1;
+}
+
+
+
+
+function ajout_scenario($dbh): void //fonction qui permet d'ajouter des scénarios à la BBD
+{
+    $idscenario = search_idscenario($dbh);
+    $idevents = recup_event($dbh);
+
+    //insertion des données dans la table scenario
+    $insertion = $dbh->prepare("insert into scenario (idscenario,nom, debut, fin, nbEv, createur) 
+                        VALUES (?,?,?,?,?,?)");
+
+    //paramètres pour la requête SQL
+    $insertion->bindparam(1, $idscenario);
+    $insertion->bindparam(2, $_POST['nom_scenario']);
+    $insertion->bindparam(3, $_SESSION['debut']);
+    $insertion->bindparam(4, $_SESSION['fin']);
+    $insertion->bindparam(5, $_POST['nbevent']);
+    $insertion->bindparam(6, $_SESSION['username']);
+    $insertion->execute();
+
+    $compt = [];
+
+
+    while(sizeof($compt) != $_POST['nbevent']){ //boucle pour l'insertion de l'association des évènements avec le scénario en question
+        $k = random_int(0, sizeof($idevents)-1);
+        $id = $idevents[$k];
+        if (!(in_array($id, $compt))){ //vérification de la donnée qui n'est pas déjà présente dans la BDD
+            $insertion_event = $dbh->prepare("insert into scenarioevenement (idscenario, idevenement) VALUES (?,?)"); //insertion des données dans la BDD
+            $insertion_event->bindparam(1, $idscenario); //paramètres de la requête
+            $insertion_event->bindparam(2, $id);
+            $insertion_event->execute();
+            $compt[]+=$id;
+        }
     }
 
 }
+
+//permet de supprimer les doublons qui se toruve dans la BDD
+function suppression_doublons_scenario($dbh): string
+{
+    $vue = $dbh->prepare("
+                        select idscenario, row_number() over (partition by debut, fin, dpi order by debut, fin, dpi) as rn
+                        from scenario");
+    $res = $vue->execute();
+    $requete = $dbh->prepare("
+                        delete
+                        from scenario
+                        using ?
+                        where ? = scenario.idscenario and ? > 1");
+    $requete->bindparam(1, $res);
+    $requete->bindparam(2, $res);
+    $requete->bindparam(3, $res);
+    $requete->execute();
+    return "";
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
 
 
 ?>
--- Partie_Scenario/Scenario.php
+++ Partie_Scenario/Scenario.php
@@ -1,7 +1,8 @@
-<html lang="en">
+<!DOCTYPE html>
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" lang="en">
     <link rel="stylesheet" href="Simulation.css" media="screen" type="text/css" />
+    <title> Page des scénarios</title>
 </head>
 <body>
 <header>
@@ -14,7 +15,7 @@
         </div>
         <div class="btn-group">
             <button onclick="location.href='principale.php'">PATIENTS</button>
-            <button>SCENARIOS</button> <!-- Bouton permettant d'accèder au scenarios -->
+            <button onclick="location.href='../Scenario/principaleEve.php'">SCENARIOS</button> <!-- Bouton permettant d'accèder au scenarios -->
             <button>JSAISPAS</button>
         </div>
     </div>
@@ -31,16 +32,16 @@
 
 
                 require ('../BDD/DataBase_Core.php');
-                require ('../Partie_Scenario/Code_php.php');
+                require ('../Partie_Scenario/Code.php');
 
                 function Connection(): PDO
                 {
                     return DataBase_Creator_Unit();
                 }
 
-                    // Zone de connexion à la base de données
+                // Zone de connexion à la base de données
                 try {
-                    $db = Connection();
+                $db = Connection();
 
                 ?>
 
@@ -49,7 +50,7 @@
                         <option id="ipp" value="defaut">--Choisissez votre DPI--</option>
                         <?php
 
-
+                        //liste déroulante pour afficher les patients
                         $recuppatient= $db->prepare('select ipp,nom, prenom from Patient');
                         $recuppatient->execute();
                         while ($row = $recuppatient->fetch(PDO::FETCH_ASSOC)) {
@@ -62,11 +63,13 @@
                         }
 
                         ?>
+                        <!-- zone de récupération des patients-->
                         <input name="valueipp" id="valueipp">
                         <input name="nomipp" id="nomipp">
                     </select>
 
                     <script>
+                        //fonction pour récupérer les données des patients
                         document.getElementById('selectDPI').addEventListener('change',function(){
                             document.getElementById('valueipp').value = this.value;
                             document.getElementById('nomipp').value = this.options[this.selectedIndex].textContent;});
@@ -82,147 +85,8 @@
                     <br><br><br>
                     <?php
 
-                    //fonction qui permet d'avoir la liste des id
-                    function id_existance($dbh){
-                        $req = $dbh->prepare("select idevenement
-                        from evenement");
-                        $req->execute();
-                        $donnees = array();
-                        foreach ($req as $r){
-                            $donnees[] = $r;
-
-                        }
-                        return $donnees;
-                    }
-
-                    function search_idscenario($db)
-                    {
-                        $src = $db->prepare("select max(idscenario)
-                        from scenario");
-                        $src->execute();
-                        $donnees = array();
-                        foreach ($src as $row){
-                            $donnees += $row;
-                        }
-                        return $donnees[0] + 1;
-                    }
-
-
-                    function recup_event_id($dbh): array
-                    {
-                        $req = $dbh->prepare("select min(idevenement), max(idevenement) from evenement");
-                        $req->execute();
-                        $infos = array();
-                        foreach ($req as $row){
-                            $infos[] = $row;
-                        }
-                        return $infos;
-                    }
-
-                    function ajout_scenario($dbh): void
-                    {
-                        $idscenario = search_idscenario($dbh);
-                        $idevents = recup_event($dbh);
-                        //insertion des données dans la table scenario
-                        $insertion = $dbh->prepare("insert into scenario (idscenario,nom, debut, fin, nbEv, createur) 
-                        VALUES (?,?,?,?,?,?)");
-
-                        $insertion->bindparam(1, $idscenario);
-                        $insertion->bindparam(2, $_POST['nom_scenario']);
-                        $insertion->bindparam(3, $_SESSION['debut']);
-                        $insertion->bindparam(4, $_SESSION['fin']);
-                        $insertion->bindparam(5, $_POST['nbevent']);
-                        $insertion->bindparam(6, $_SESSION['username']);
-                        $insertion->execute();
-
-                        $compt = [];
-
-                        while(sizeof($compt) != $_POST['nbevent']){
-                            $k = random_int(0, sizeof($idevents)-1);
-                            $id = $idevents[$k];
-                            if (!(in_array($id, $compt))){
-                                $insertion_event = $dbh->prepare("insert into scenarioevenement (idscenario, idevenement) VALUES (?,?)");
-                                $insertion_event->bindparam(1, $idscenario);
-                                $insertion_event->bindparam(2, $id);
-                                $insertion_event->execute();
-                                $compt[]+=$id;
-                                print_r($compt);
-                            }
-                        }
-
-                    }
-
-                    function search_idevent($db)
-                    {
-                        $src = $db->prepare("select max(idevenement)
-                        from evenement");
-                        $src->execute();
-                        $donnees = array();
-                        foreach ($src as $row){
-                            $donnees += $row;
-                        }
-                        return $donnees[0] + 1;
-                    }
-
-                    function hdeclenchement(): int
-                    {
-                        return rand(0,23);
-                    }
-
-
-
-                    function suppression_doublons_scenario($dbh): string
-                    {
-                        $vue = $dbh->prepare("
-                        select idscenario, row_number() over (partition by debut, fin, dpi order by debut, fin, dpi) as rn
-                        from scenario");
-                        $res = $vue->execute();
-                        $requete = $dbh->prepare("
-                        delete
-                        from scenario
-                        using ?
-                        where ? = scenario.idscenario and ? > 1");
-                        $requete->bindparam(1, $res);
-                        $requete->bindparam(2, $res);
-                        $requete->bindparam(3, $res);
-                        $requete->execute();
-                        return "";
-                    }
-
-                    //fonction qui recup les id min et max
-
-
-
-
-                    //fonction qui permet de recup les events en fonction de leur id
-                    function recup_event($dbh){
-                        $i = $_SESSION['nb_event'];
-                        $k= 0;
-
-                        //print_r(recup_event_id($dbh));
-                        $donnees = recup_event_id($dbh);
-                        $les_ids= id_existance($dbh);
-                        $events = array();
-                        print_r($les_ids);
-                        print_r($donnees);
-                        while ($k != $i){
-                            $r = random_int($donnees[0][0], $donnees[0][1]);
-                            if ( in_array($r, $les_ids[$k])){
-                                $events[] = $r;
-                                $k += 1;
-                            }
-                        }
-                        return $events;
-                    }
-
-                    //creer fonction pour ajout des events et des scenarios
-                    //ajouter l'utilisateur pour les scenarios
-                    //relier les pages pour la recup
-                    //recup les donnees sur discord(jeu de test scenario)
-
+                    //Main
                     if(!empty($db)) {
-                        //print_r(recup_event_id($db));
-                        //print_r(recup_event($db));
                         if (!empty($_SESSION['val']) && !empty($_SESSION['debut']) && !empty($_SESSION['fin'])) {
                             ajout_scenario($db);
                             echo "<P style='color: green'>l'ajout a été effectué</p>";
@@ -230,82 +94,32 @@
 
                     }
 
-
-
-
-
-
+                    //gestion en cas d'exception
                     }catch (PDOException $e) {
                         echo 'Erreur : ' . $e->getMessage();
                     }
 
                     ?>
-                    <!--
-                    <script type="text/javascript">
-
-
-                    let i = 0;
-                    let contenu = "";
-
-                    function nouveauInput(){
-                        i = i + 1;
-                        contenu = contenu + "<label for='event" + i + "'>Evenement "+ i +" <input type='text' id='event" + i + "' /><br /> ";
-                        document.getElementById('auto').innerHTML = contenu;
-                    }
-
-                    function event_aleatoire(min, max){
-                        let k=0;
-                        let i=0
-                        let n = document.getElementById('nbevent');
-                        let list = [];
-                        while (k < n){
-                            let r = Math.floor(Math.random() * (max-min))+min;
-                            list.push(r);
-                            //ajouter l'event correspondant au numéro des variables
-                        }
-                        while (i < list.length){
-                            contenu = contenu + list[i];
-                        }
-                        document.getElementById('auto').innerHTML = contenu;
-                    }
-
-                    function supprimerInput() {
-                        const e = document.getElementById("event");
-                        let child = e.lastElementChild;
-                        while (child) {
-                            e.removeChild(child);
-                            child = e.lastElementChild;
-                        }
-                    }
-
-                    const btn = document.getElementById(
-                        "btn").onclick = function () {
-                        supprimerInput();
-                    };
-
-                    </script>
-                    -->
 
                     <div id="ajout_event">
-                        <input type="text" id="nbevent" name="nbevent" placeholder="Entrez le nombre d'event que vous voulez" style="width: 50%">
-                        <input type="button" id="btn" value="Reset les informations" onclick="window.location.reload()" >
+                        <div id="event_categorie" style="background-color: #66CCCC">
+                            <label for="nbevent">Nombre d'évènement</label> <br> <input type="text" id="nbevent" name="nbevent" placeholder="Entrez le nombre d'event que vous voulez" style="width: 50%"><br>
+                            <label for="type_categorie">Catégorie des évènements</label> <br> <input type="text" id="type_categorie" name="type_categorie" placeholder="Entrez la catégorie d'évènement que vous souhaitez" style="width: 50%">
+                        </div>
+
+                        <br><br>
+
+                        <div id="event_aleatoire" style="background-color: mediumpurple">
+                            <label for="nbevent_alea">Nombre d'évènement</label> <br> <input type="text" id="nbevent_alea" name="nbevent_alea" placeholder="Entrez le nombre d'event que vous voulez" style="width: 50%">
+                        </div>
                     </div>
                     <br><br><br>
-
-                        <!--<button type="button" onclick="nouveauInput()">Ajouter un évènement</button>-->
-
-                    <div id="auto">
-                        <!-- partie pour les ajouts auto des inputs des events-->
-                    </div>
-
-
 
                     <div align="center" id="stock">
                         <br><br>
                         <button id="ajout_s" value="Ajout_s" style="position: center">Ajouter le scenario</button>
                         <button type="reset" id="reset_s" value="reset_s"> Reset les informations</button>
-                        <?php
-                        ?>
+
 
                     </div>
                 </div>
