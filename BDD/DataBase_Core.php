87156c3e7b4251c4dbea789789826fe24bebe88d
AnselotSteven <Steven.Anselot@uphf.fr>
2023-01-10 12:03:44 +0100

    changement scenario et fonction ajout scenario

--- BDD/DataBase_Core.php
+++ BDD/DataBase_Core.php
@@ -1,17 +1,17 @@
 <?php
 
-function DataBase_Creator_Unit()
+function DataBase_Creator_Unit(): PDO
 {
     //Zone de connexion à la base de données
-    $db_username = 'theo';
-    $db_password = 'theo';
+    $db_username = 'postgres';
+    $db_password = 'steven59330';
     $db_name = 'postgres';
     $db_host = 'localhost';
     
     return new PDO("pgsql:host=$db_host;port=5432;dbname=$db_name;user=$db_username;password=$db_password");
 }
 
-function Hasher($val,$hashed)
+function Hasher($val,$hashed): string
 {
     $options = [ 'cost' => $val, ];
     return password_hash($hashed,PASSWORD_BCRYPT, $options);
